6c43e8c0863432fa93c20e69e5762a19dc2a978c
Abderrazek <abderrazek.younsi@enovos.eu>
2025-11-27 22:24:52 +0100

    :art: simplify status bar configuration and remove unused dark mode logic

--- App.tsx
+++ App.tsx
@@ -5,18 +5,16 @@
  */
 
 import React from 'react';
-import { StatusBar, useColorScheme } from 'react-native';
+import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AppNavigator from './src/navigation/AppNavigator';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <StatusBar barStyle="light-content" backgroundColor="#000000" translucent />
         <AppNavigator />
       </SafeAreaProvider>
     </GestureHandlerRootView>
